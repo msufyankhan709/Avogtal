ab7c37a68e2a63e39a1c022cfb752b3ab97da4fe
msufyankhan709 <msufyankhan709@gmail.com>
2022-02-23 14:22:43 +0500

    initialization

---
+++ src/components/vapp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <home />
+  </div>
+</template>
+
+<script>
+import home from './home.vue';
+export default {
+  name:'vapp',
+  components:{
+      home,
+  }
+}
+</script>
+
+<style>
+
+</style>
